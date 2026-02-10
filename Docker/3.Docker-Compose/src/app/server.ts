455a5ae01a86b206b1368a15885275a93794c5ce
Kirtan Nahar <notkirtann@outlook.com>
2026-02-10 11:30:35 +0530

    docker compose

--- Docker/3.Docker-Compose/src/app/server.ts
+++ Docker/3.Docker-Compose/src/app/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import type {Response,Request} from 'express'
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
   return res.json({
     message: `Hello from Typescript Server`,
   });
